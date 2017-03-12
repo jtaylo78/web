ac3e1c1bc4b81417bc85b5c5419d93dc84885137
james <jtaylo78@gmail.com>
2017-03-12 15:44:44 -0600

    WIP

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,30 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, flash
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 import MySQLdb
 from flask import current_app, render_template
-
-
 from . import main
 #from . import NameForm
 from .. import db
 from ..models import User
+def getFullTextData(search_string):
+    app = current_app._get_current_object()
+    db = MySQLdb.connect(host=app.config['SPHINX_SERVER'],port=app.config['SPHINX_PORT'])
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT * from test1 where Match('%s')" % (search_string))
+    rows = cursor.fetchall()
+
+    db.close()
+    return rows
+
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
      form = NameForm()
@@ -26,32 +39,25 @@ def index():
 @main.route('/search', methods=['GET', 'POST'])
 def search_main():
      form = SearchForm()
-     search_string = None
+
+     #flash('Loading')
      if form.validate_on_submit():
         session['search_string'] = form.search.data
-        #session['search_data']
-        getFullTextData('cmd')
+
+        #flash('Looks like you have changed your name!')
         return redirect(url_for('.search_main'))
+     form.search.data = session.get('search_string', False)
      return render_template('search.html',
              form=form, search_string=session.get('search_string'),
-             known=session.get('known', False),
+             search_data=getFullTextData(session.get('search_string')),
+             sknown=session.get('known', False),
              current_time=datetime.utcnow())
 
 
-def getFullTextData(search_string):
-    app = current_app._get_current_object()
-    db = MySQLdb.connect(host=app.config['SPHINX_SERVER'],port=app.config['SPHINX_PORT'])
-
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # execute SQL query using execute() method.
-    cursor.execute("SELECT * from test1")
-
 class NameForm(Form):
- name = StringField('What is your name?', validators=[Required()])
+ name = StringField('What is your name?', validators=[DataRequired()])
  submit = SubmitField('Submit')
 
 class SearchForm(Form):
- search = StringField('Search', validators=[Required()])
+ search = StringField('Search', validators=[DataRequired()])
  submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
-     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hardtoguessstring'
      SQLALCHEMY_COMMIT_ON_TEARDOWN = True
      FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
      FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
